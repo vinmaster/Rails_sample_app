fc34e25ebc2b68938cd9343ce7ab055a016846e8
Vincent Ho <vinmaster99@yahoo.com>
2012-12-20 15:45:10 -0800

    Add Cucumber features

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,1 +1,8 @@
 include ApplicationHelper
+
+# Use to replace code in spec files ':have_error_message'
+RSpec::Matchers.define :have_error_message do |message|
+	match do |page|
+		page.should_selector('div.alert.alert-error', text: 'Invalid')
+	end
+end
